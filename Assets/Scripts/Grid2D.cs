780aa0ae43bfa11a0b311232dc1b7b2dcce46495
Isobel McNeany <icmc112@yahoo.co.uk>
2018-11-13 22:06:37 +0000

    allowed the user to click to add a density point

---
+++ Assets/Scripts/Grid2D.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid2D : MonoBehaviour {
+    public int width;
+    public int height;
+    public GameObject pointData;
+	// Use this for initialization
+	void Start () {
+        DrawGrid();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Debug.Log("cast");
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                //agent.destination = hit.point;
+                
+                if(hit.collider.gameObject.GetComponent<PointData>())
+                {
+                    PointData point = hit.collider.gameObject.GetComponent<PointData>();
+                    point.SetDensitySource(new Vector2(hit.point.x, hit.point.z));
+                    Debug.Log("mouse pos" + hit.point.x + hit.point.z);
+
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for(int i = 0; i< width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+             //   Gizmos.DrawSphere(new Vector3(i, 0, j), 0.03f);
+            }
+        }
+    }
+
+    void DrawGrid()
+    {
+        for (int i = 0; i <= width; i++)
+        {
+            for (int j = 0; j <= height; j++)
+            {
+                Instantiate(pointData,new Vector3(i, 0,j), new Quaternion(1.0f,0,0,1));
+                
+            }
+        }
+    }
+
+
+}
--- Assets/Scripts/PointData.cs
+++ Assets/Scripts/PointData.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class PointData : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    float[] density;
+    float[] prevDensity;
+    Vector2[] velocity;
+    Vector2[] prevVelocity;
+    public List<Vector2> densitySources = null;
+
+
+    // Use this for initialization
+    void Start () {
+        
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+    public float GetDensity(int i)
+    {
+        return density[i];
+    }
+
+    public float GetPreviousDensity(int i)
+    {
+        return prevDensity[i];
+    }
+
+    public void SetPreviousDensity(int i, float prevD)
+    {
+        prevDensity[i] = prevD;
+    }
+
+    public void SetDensity(int i, float dens)
+    {
+        prevDensity[i] = dens;
+    }
+
+    public Vector2 GetVelocity(int i)
+    {
+        return velocity[i];
+    }
+
+    public Vector2 GetPreviousVelocity(int i)
+    {
+        return prevVelocity[i];
+    }
+
+    public void SetPreviousVelocity(int i, Vector2 prevV)
+    {
+        prevVelocity[i] = prevV;
+    }
+
+    public void SetVelocity(int i, Vector2 vel)
+    {
+        prevVelocity[i] = vel;
+    }
+
+    public Vector2 GetDensitySource(int i)
+    {
+        return densitySources[i];
+    }
+
+    public void SetDensitySource(Vector2 source)
+    {
+        //take the 0. to get the touch on the square
+        densitySources.Add(source);
+        //densitySources.Add(source);
+        Debug.Log("source" + source);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
